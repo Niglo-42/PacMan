d0f9f4da24d992af510c2a1a4241a37005575e8d
tom <thomas@Laptop-Niglo.localdomain>
2026-09-04 08:57:48 +0200

    ajout des togglessssssss

--- src/interface/menu.py
+++ src/interface/menu.py
@@ -6,21 +6,26 @@ class ToggleBox:
         self.name = string
         self.font = pygame.font.Font("font/press_start_2p.ttf", size)
         self.text = self.font.render(string, False, "#dedeff")
-        self.bool = self.font.render(str(boolean), False, "#0ec43c" if boolean else "#ff0000")
+        self.bool_val = boolean
+        self.bool = self.font.render("ON" if boolean else "OFF",
+                                      False,
+                                    "#0ec43c" if boolean else "#ff0000")
         self.t_size_w, self.t_size_h = self.text.get_size()
         self.b_size_w, self.b_size_h = self.bool.get_size()
         self.surf = pygame.Surface((self.t_size_w + self.b_size_w + 10, self.t_size_h))
         self.draw_box()
 
     def flip(self):
-        self.value = not self.value
+        self.bool_val = not self.bool_val
+        self.bool = self.font.render(
+            "ON" if self.bool_val else "OFF",
+            False, "#0ec43c" if self.bool_val else "#ff0000")
+        self.b_size_w, self.b_size_h = self.bool.get_size()
 
-    # def draw_box(self):
-    #     self.surf.fill(0)
-    #     self.surf.blit(self.range_box, (self.t_size_w + 10 , 0))
-    #     self.surf.blit(self.text, (0, 0))
-    #     rect = self.font_val.get_rect(midtop=(self.t_size_w + 10 + range_size // 2, 0))
-    #     self.surf.blit(self.font_val, rect)
+    def draw_box(self):
+        self.surf.fill(0)
+        self.surf.blit(self.text, (0 , 0))
+        self.surf.blit(self.bool, (self.t_size_w + 10, 0))
 
 class ParamBox:
     def __init__(self, string, size, min_v, max_v, range_size, value):
@@ -76,9 +81,11 @@ class Menu:
         self.render.screen.fill(0)
         toggles = ("cheat_mode", "audio_enable")
         boxes, toggle_boxes = [], []
-        nb_boxes = len(clamps.keys())
+        nb_boxes = len(clamps.keys()) + len(toggles)
         font_size = self.h // (nb_boxes * 4 + 2)
         range_size = self.render.screen.get_size()[0] // 10
+        for toggle in toggles:
+            toggle_boxes.append(ToggleBox(toggle, font_size, False))
         for name, (min_v, max_v) in clamps.items():
             current = config.get(name, min_v)
             boxes.append(RangeBox(name, font_size, min_v, max_v, range_size, current))
@@ -93,11 +100,15 @@ class Menu:
                         for box in boxes:
                             if box.name == key:
                                 config[key] = box.value
+                        for tog in toggle_boxes:
+                            if tog.name == key:
+                                config[key] = tog.bool_val
                     return config
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mx, my = event.pos
-                    for i, box in enumerate(boxes, 0):
-                        rect = box.surf.get_rect(center=((pad_w, i * (font_size * 2) + pad_h)))
+                    j = 0
+                    for box in boxes:
+                        rect = box.surf.get_rect(center=((pad_w, j * (font_size * 2) + pad_h)))
                         rect.x += (box.text.get_size()[0] + 10)
                         r_box = box.range_box.get_rect()
                         r_box.x += rect.x
@@ -110,9 +121,26 @@ class Menu:
                             box.value = val
                             box.font_val = box.font.render(str(val), False, "#181713")
                             box.draw_box(range_size)
-            for i, box in enumerate(boxes, 0):
+                        j += 1
+                    for tog in toggle_boxes:
+                        rect = tog.surf.get_rect(center=((pad_w, j * (font_size * 2) + pad_h)))
+                        rect.x += (tog.text.get_size()[0] + 10)
+                        r_bool = tog.bool.get_rect()
+                        r_bool.x += rect.x
+                        r_bool.y += rect.y
+                        j += 1
+                        if collide_point(r_bool, mx, my):
+                            tog.flip()
+                            tog.draw_box()
+            i = 0
+            for box in (boxes):
                 rect = box.surf.get_rect(center=((pad_w, i * (font_size * 2) + pad_h)))
                 self.render.screen.blit(box.surf, rect)
+                i += 1
+            for tog in (toggle_boxes):
+                rect = tog.surf.get_rect(center=((pad_w, i * (font_size * 2) + pad_h)))
+                self.render.screen.blit(tog.surf, rect)
+                i += 1
             pygame.display.flip()
             clock.tick(fps)
 
